70549b6293937287d80bbb12cfd5d75a1f1a46b2
webstudio-ma <assadinizahrae@gmail.com>
2026-08-25 10:56:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    alert("Merci ! Votre message est prêt à être envoyé.");
+
+});
